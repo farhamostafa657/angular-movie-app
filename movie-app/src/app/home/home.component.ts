58d8825aa7f9a08bdc1d53b1070e911cb04cf6ee
Your Name <farhamostafa657@gmail.com>
2025-02-18 16:12:30 +0400

    generate home&navbar&searchbar&movielist components

---
+++ movie-app/src/app/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { SearchComponent } from '../search/search.component';
+import { MovieListComponent } from '../movie-list/movie-list.component';
+
+@Component({
+  selector: 'app-home',
+  imports: [NavbarComponent, SearchComponent, MovieListComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
+})
+export class HomeComponent {}
